6645f4e5ce87e9d5fed36551d8d64b867e73231c
Alvaro Morales <amorales@integralit.cl>
2022-08-25 02:48:06 -0400

    Feature: Products REST

--- routes/products.js
+++ routes/products.js
@@ -1,30 +1,129 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/product.js");
+const Category = require("../models/category.js");
+const mongoose = require("mongoose");
+
+/* router.get("/", async (req, res) => {
+  const products = await Product.find().select("name image ");
+
+  if (!products) res.status(500).send({ success: false });
+
+  res.send(products);
+}); */
 
 router.get("/", async (req, res) => {
-  const products = await Product.find();
+  let filter = {};
+
+  if (req.query.categories) {
+    filter = { category: req.query.categories.split(",") };
+  }
+  const products = await Product.find(filter).populate("category");
 
   if (!products) res.status(500).send({ success: false });
 
   res.send(products);
 });
 
-router.post("/", (req, res) => {
-  const product = new Product({
+router.post("/", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
+
+  let product = new Product({
     name: req.body.name,
-    image: req.body.image,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image, // "http://localhost:3000/public/upload/image-2323232"
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
 
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
+  product = await product.save();
+
+  if (!product) return res.status(500).send("The product cannot be created");
+
+  res.status(201).json(product);
+});
+
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id).populate("category");
+
+  if (!product) res.status(500).send({ success: false });
+
+  res.send(product);
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
+
+  let product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image, // "http://localhost:3000/public/upload/image-2323232"
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
+
+  product = await product.save();
+
+  if (!product) return res.status(404).send("Product cannot be created");
+
+  res.send(product);
+});
+
+router.delete("/:id", (req, res) => {
+  Product.findByIdAndRemove(req.params.id)
+    .then((product) => {
+      if (product) {
+        return res
+          .status(200)
+          .send({ success: true, message: "The product has been deleted." });
+      } else {
+        return res
+          .status(404)
+          .send({ success: false, message: "The product cannot be deleted." });
+      }
     })
     .catch((err) => {
-      res.status(500).json({ success: false, error: err });
+      return res.status(400).json({ success: false, error: err });
     });
+});
+
+router.get("/get/count", async (req, res) => {
+  const productCount = await Product.countDocuments();
+
+  if (!productCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    productCount: productCount,
+  });
+});
+
+router.get("/get/featured/:count", async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  const productCount = await Product.find({ isFeatured: true }).limit(+count);
+
+  if (!productCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send(productCount);
 });
 
 module.exports = router;
